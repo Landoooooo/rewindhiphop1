27b8bf74727d2215b9f2eba18ddc740a244f4595
Alando Appling <alandoappling@gmail.com>
2017-11-10 16:50:42 -0800

    Updated gemfile and application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,4 +12,8 @@ module Rewindhiphop
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+
+	config.assets.precompile += Ckeditor.assets
+	config.assets.precompile += %w( ckeditor/* )
+	config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 end
