4946a4f23e5b2736edc840a8ac30004d66097152
Alando Appling <alandoappling@gmail.com>
2017-11-12 11:44:57 -0800

    Updated Post controller so you don't have to be signed in to view posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
 	 before_action :find_post, only: [:edit, :update, :show, :delete]
-   before_action :authenticate_user!
    before_action :only => [:new, :edit] do 
     redirect_to new_user_session_path unless current_user && current_user.admin
    end
