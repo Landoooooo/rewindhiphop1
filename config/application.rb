e768f35602382ae64b9071a14591cb0d1e7fc082
Alando Appling <alandoappling@gmail.com>
2017-11-13 18:25:12 -0800

    Heroku Ckeditor fix

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ module Rewindhiphop
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.precompile += Ckeditor.assets
-	config.assets.precompile += %w( ckeditor/* )
-	config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+	config.assets.precompile += Ckeditor.assets
+	config.assets.precompile += %w(ckeditor/*)
   end
 end
  
